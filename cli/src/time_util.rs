c01d9993e2f1ad48a5bd903ed76b73438c68dbbd
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-01-27 20:12:28 +0100

    formatter: use jiff for duration formatting

--- cli/src/time_util.rs
+++ cli/src/time_util.rs
@@ -4,6 +4,8 @@ use chrono::FixedOffset;
 use chrono::LocalResult;
 use chrono::TimeZone as _;
 use chrono::Utc;
+use jiff::Unit;
+use jiff::ZonedDifference;
 use jj_lib::backend::Timestamp;
 use once_cell::sync::Lazy;
 use thiserror::Error;
@@ -84,14 +86,47 @@ pub fn format_absolute_timestamp_with(
     Ok(datetime.format_with_items(format.items.iter()).to_string())
 }
 
+fn timestamp_to_jiff(value: &Timestamp) -> Result<jiff::Zoned, jiff::Error> {
+    let tz = jiff::tz::TimeZone::fixed(jiff::tz::Offset::from_seconds(value.tz_offset * 60)?);
+    let timestamp = jiff::Timestamp::new(
+        value.timestamp.0.div_euclid(1000),
+        (value.timestamp.0.rem_euclid(1000)) as i32 * 1000000,
+    )?;
+    Ok(timestamp.to_zoned(tz))
+}
+
 pub fn format_duration(
     from: &Timestamp,
     to: &Timestamp,
-    format: &timeago::Formatter,
+    _format: &timeago::Formatter,
 ) -> Result<String, TimestampOutOfRange> {
-    let duration = datetime_from_timestamp(to)?
+    let _duration = datetime_from_timestamp(to)?
         .signed_duration_since(datetime_from_timestamp(from)?)
         .to_std()
         .map_err(|_: chrono::OutOfRangeError| TimestampOutOfRange)?;
-    Ok(format.convert(duration))
+
+    let a = timestamp_to_jiff(from).unwrap();
+    let b = timestamp_to_jiff(to).unwrap();
+
+    let duration = b.duration_since(&a);
+    let (unit_min, unit_max) = if duration.as_hours() > 23 {
+        (Unit::Day, Unit::Year)
+    } else if duration.as_hours() > 0 {
+        (Unit::Hour, Unit::Hour)
+    } else if duration.as_mins() > 0 {
+        (Unit::Minute, Unit::Minute)
+    } else {
+        (Unit::Second, Unit::Second)
+    };
+
+    let b = b.with_time_zone(a.time_zone().clone());
+    let span = a
+        .until(
+            ZonedDifference::new(&b)
+                .smallest(unit_min)
+                .largest(unit_max),
+        )
+        .unwrap();
+
+    Ok(format!("{:#?} ago", span))
 }
